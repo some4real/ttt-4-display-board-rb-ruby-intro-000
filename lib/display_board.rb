fcb2ddf08f302224396ce19276c6e5c1e5d42cfc
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2020-11-06 04:36:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
- puts "#{0}, #{1}, #{2}, #{3}, #{4}, #{5}, #{6}, #{7}, #{8}"
+ puts "#{board[0]}, #board[{1}], #{board[2]}, #{board[3}], #{board[4}], #{board[5}], #{board[6}], #{board[7}], #{board[8}]"
  end
