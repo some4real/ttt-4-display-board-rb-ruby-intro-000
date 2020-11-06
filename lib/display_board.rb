3dc530b3b1996653aa325691d8173ca0abdb4397
Oluwaseyi Olufemi <oluwaseyi.olufemi.e@gmail.com>
2020-11-06 04:31:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+ puts "#{0}, #{1}, #{2}, #{3}, #{4}, #{5}, #{6}, #{7}, #{8}"
+ end
